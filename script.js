d89ff9931500c373ca74bb6b3f278aab280a1de7
Diyar Aslan <echoaslan11@gmail.com>
2025-01-15 13:56:35 +0100

    verbessert

--- script.js
+++ script.js
@@ -1,45 +1,89 @@
 
 
 
-function arithmetik_Operation(valu){
-const operation  = valu;
-    switch(operation){
-        case "+":
-            let sum = parseInt(first)  + parseInt(second)
-            console.log(sum)
-            reset();
-             document.getElementById("out").textContent = sum;
-             break;
-            
-        case "-":
-            let sub = parseInt(first)  -  parseInt(second)
-            console.log(sub)
-            reset();
-             document.getElementById("out").textContent = sub;
-             break;
-        case "*":
-            let mult = parseInt(first)  *  parseInt(second)
-            console.log(mult)
-            reset();
-             document.getElementById("out").textContent = mult;
-             break
-    }
+function arithmetik_Operation(valu) {
+  let operation = valu;
+  switch (operation) {
+    case "+":
+      if(first === 0){
+        alert("First chose a number")
+        break;
+      }
+      myArray[1] = "+"
+      document.getElementById("inhalt").textContent = first  + operation ;
+      break;
+    case "-":
+      myArray[1] = "-"
+      document.getElementById("inhalt").textContent = first  + operation ;
+      break;
+    case "*":
+      myArray[1] = "*"
+      document.getElementById("inhalt").textContent = first  + operation ;
+      break;
+
+  
+  }
+}
    
+let myArray = [];
+
+let first = " ";
+let second = " ";
+let default1 = " ";
+
+function takeValue(val) {
+  if(first === default1){
+    myArray[0] = val;
+    first = val;
+    document.getElementById("inhalt").textContent = val;
+    document.getElementById("out").textContent = " ";
+  }
+  else{  
+    second = val;
+    myArray[2] = second
+    document.getElementById("inhalt").textContent = first  + myArray[1] + second ;
+    executeOperation()
   }
 
-  let first = 0;
-  let second = 0;
+    
+  
+}
 
-  function takeValue(val){
-    if(first === 0  ){
-        first = val;
-    }else{
-        second = val;
-    }
+function executeOperation(){
+  switch (myArray[1]) {
+    case "+":
+      let sum = parseInt(myArray[0]) + parseInt(myArray[2])
+      document.getElementById("out").textContent = sum;
+      reset();
+      break;
+
+    case "-":
+      let sub = parseInt(first) - parseInt(second)
+      reset();
+      document.getElementById("out").textContent = sub;
+      break;
+    case "*":
+      let mult = parseInt(first) * parseInt(second)
+      reset();
+      document.getElementById("out").textContent = mult;
+      break;
   }
-  function reset(){
-    first = 0;
-    second = 0;
-  }
+
+
+
+}
+
+function reset() {
+
+  first = " ";
+  second = " ";
+}
+function restart(){
+  document.getElementById("out").textContent = " ";
+
+  document.getElementById("inhalt").textContent = " ";
+
+
+}
 
 
